45f33b0b1773e089a27a712e6f4f5514d8535b51
Raphael Orati <misterbroderkill@gmail.com>
2026-03-27 18:07:07 -0300

    feat: Inclusão de  lógica de negócio, com chamada de API utilizando OpenFeing

---
+++ Projeto_Spring_Cep_Clima/src/main/java/br/com/orati/cepclima/service/API/CepApiService.java
@@ -0,0 +1,14 @@
+package br.com.orati.cepclima.service.API;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.orati.cepclima.model.Cep;
+
+@FeignClient(name = "cep", url = "https://cep.awesomeapi.com.br")
+public interface CepApiService {
+
+    @GetMapping("/json/{cep}")
+    public Cep buscarDadosCep(@PathVariable("cep") String cepDTO);
+}
