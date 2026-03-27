071a300d804aea7490ecbeb4e3b2307d092c92c9
Raphael Orati <misterbroderkill@gmail.com>
2026-03-27 18:07:47 -0300

    feat: Inclusão de Exceptions únicas e handler para tratamento de erros e falhas globais.

---
+++ Projeto_Spring_Cep_Clima/src/main/java/br/com/orati/cepclima/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package br.com.orati.cepclima.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import br.com.orati.cepclima.exceptions.CepInvalidoException;
+import br.com.orati.cepclima.exceptions.CepVazioException;
+import br.com.orati.cepclima.model.DTOs.RestErrorResponseDTO;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(CepInvalidoException.class)
+    public ResponseEntity<RestErrorResponseDTO> handlerCepInvalido(CepInvalidoException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        RestErrorResponseDTO response = new RestErrorResponseDTO("CEP inválido.", status);
+        return ResponseEntity.status(status).body(response);
+    }
+
+    /**
+     * 
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(CepVazioException.class)
+    public ResponseEntity<RestErrorResponseDTO> handlerCepVazio(CepVazioException ex) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        RestErrorResponseDTO response = new RestErrorResponseDTO("CEP Vazio.", status);
+        return ResponseEntity.status(status).body(response);
+    }
+
+    /**
+     * 
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<RestErrorResponseDTO> handlerRotaNaoEncontrada(NoResourceFoundException ex) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        RestErrorResponseDTO response = new RestErrorResponseDTO("NOT_FOUND", status);
+        return ResponseEntity.status(status).body(response);
+    }
+}
